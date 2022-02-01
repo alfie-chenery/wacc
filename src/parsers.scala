36488fcfab3cc4f359055270a3465b4b162a4a41
James Lee-Jones <jl8320@ic.ac.uk>
2022-02-01 15:04:00 +0000

    Implemented basis for parsers.scala

--- src/parsers.scala
+++ src/parsers.scala
@@ -1,4 +1,9 @@
-import parsley.Parsley, Parsley._
+import parsley.Parsley
+import Parsley._
+import parsley.character.{noneOf, oneOf, string}
+import parsley.combinator.{eof, many}
+import parsley.implicits.character.charLift
+import parsley.implicits.character.stringLift
 
 /*
 ⟨program ⟩      ::=   ‘begin’  ⟨func⟩*  ⟨stat ⟩  ‘end’
@@ -67,3 +72,57 @@ import parsley.Parsley, Parsley._
 ⟨pair-liter ⟩     ::=   ‘null’
 ⟨comment ⟩        ::=   ‘#’  (any-character-except-EOL)*  ⟨EOL⟩
 */
+
+lazy val program = "begin" ~> many(func) ~> stat <~ "end" <~ eof
+
+lazy val func = _type ~> ident ~> '(' ~> (noneOf(param_list) <|> oneOf(param_list)) ~> ')' ~> "is" ~> stat <~ "end"
+
+lazy val param_list = param <~> many(',' <~> param)
+
+lazy val param = _type <-> ident
+
+lazy val stat = attempt("skip") <|>
+                _type ~> ident ~> '=' ~> assign_rhs <|>
+                assign_lhs ~> '=' ~> assign_rhs <|>
+                "read" = assign_lhs <|>
+                "free" ~> expr <|>
+                "return" ~> expr <|>
+                "exit" ~> expr <|>
+                "print" ~> expr <|>
+                "println" ~> expr <|>
+                "if" ~> expr ~> "then" ~> stat
+
+lazy val assign_lhs = ???
+lazy val assign_rhs = ???
+lazy val arg_list = ???
+lazy val pair_elem = ???
+lazy val _type = ???
+lazy val base_type = ???
+lazy val array_type = ???
+lazy val pair_type = ???
+lazy val pair_elem_type = ???
+lazy val expr = ???
+lazy val unary_oper = ???
+lazy val binary_oper = ???
+lazy val ident = ???
+lazy val array_elem = ???
+lazy val int_liter = ???
+lazy val digit = ???
+lazy val int_sign = ???
+lazy val bool_liter = ???
+lazy val char_liter = ???
+lazy val str_liter = ???
+lazy val character = ???
+lazy val escaped_char = ???
+lazy val array_liter = ???
+lazy val pair_liter = ???
+lazy val comment = ???
+
+
+
+
+|        ‘if’  ⟨expr ⟩  ‘then’  ⟨stat ⟩  ‘else’  ⟨stat ⟩  ‘fi’
+|        ‘while’  ⟨expr ⟩  ‘do’  ⟨stat ⟩  ‘done’
+|        ‘begin’  ⟨stat ⟩  ‘end’
+|        ⟨stat ⟩  ‘;’  ⟨stat ⟩
+
