1a00c1d57f59b220c882ae6e2762a3937daf290b
sg520 <sg520@ic.ac.uk>
2022-02-11 15:56:54 +0000

    [sg520] expression printer for error messages

---
+++ src/main/scala/parsers/ExpressionPrinter.scala
@@ -0,0 +1,95 @@
+package parsers
+
+import parsers.Ast._
+
+class ExpressionPrinter {
+
+  def prettyPrint(node: AstNode) : String = {
+    node match{
+      case Skip => "skip"
+      case Decl(t, i, rhs) => prettyPrint(t) + prettyPrint(i) + " = " + prettyPrint(rhs)
+      case Assign(lhs, rhs) => prettyPrint(lhs) + " = " + prettyPrint(rhs)
+      case Read(lhs) => "read " + prettyPrint(lhs)
+      case Free(expr) => "free " + prettyPrint(expr)
+      case Return(expr) => "return " + prettyPrint(expr)
+      case Exit(expr) => "exit " + prettyPrint(expr)
+      case Print(expr) => "print " + prettyPrint(expr)
+      case Println(expr) => "println " + prettyPrint(expr)
+      case NewPair(fst, snd) => "newpair(" + prettyPrint(fst) + "," + prettyPrint(snd) + ")"
+      case Call(Ident(i), ArgList(al)) =>
+        val sb = new StringBuilder("call")
+        sb.append(i)
+        sb += '('
+        if (al.nonEmpty) {
+          if (al.length > 1) {
+            for (i <- al.indices) {
+              sb.append(al(i))
+              sb += ','
+            }
+          }else{
+            sb.append(al.head)
+          }
+        }
+        sb += ')'
+        sb.toString()
+      case FstPair(fst) => "fst " + prettyPrint(fst)
+      case SndPair(snd) => "snd " + prettyPrint(snd)
+      case WInt => "int"
+      case WBool => "bool"
+      case WChar => "char"
+      case WString => "string"
+      case ArrayType(t) => prettyPrint(t)
+      case Pair => "pair"
+      case IntLiter(i) => i.toString
+      case BoolLiter(b) => b.toString
+      case CharLiter(c) => c.toString
+      case StrLiter(s) => s
+      case PairLiter => "null"
+      case Ident(i) => i
+      case ArrayElem(Ident(i), exprs) =>
+        val sb = new StringBuilder(i)
+        sb.append(" = [")
+        for (i <- exprs.indices){
+          sb.append(exprs(i))
+          sb.append(", ")
+        }
+        sb += ']'
+        sb.toString()
+      case ParensExpr(expr) => "(" + prettyPrint(expr) + ")"
+      case Or(l, r) => prettyPrint(l) + " || " + prettyPrint(r)
+      case And(l, r) => prettyPrint(l) + " && " + prettyPrint(r)
+      case Eq(l, r) => prettyPrint(l) + " == " + prettyPrint(r)
+      case NotEq(l, r) => prettyPrint(l) + " != " + prettyPrint(r)
+      case Greater(l, r) => prettyPrint(l) + " > " + prettyPrint(r)
+      case GreaterEq(l, r) => prettyPrint(l) + " >= " + prettyPrint(r)
+      case Less(l, r) => prettyPrint(l) + " < " + prettyPrint(r)
+      case LessEq(l, r) => prettyPrint(l) + " <= " + prettyPrint(r)
+      case Plus(l, r) => prettyPrint(l) + " + " + prettyPrint(r)
+      case Minus(l, r) => prettyPrint(l) + " - " + prettyPrint(r)
+      case Mult(l, r) => prettyPrint(l) + " * " + prettyPrint(r)
+      case Div(l, r) => prettyPrint(l) + " / " + prettyPrint(r)
+      case Mod(l, r) => prettyPrint(l) + " % " + prettyPrint(r)
+      case Not(e) => "!" + prettyPrint(e)
+      case Negate(e) => "-" + prettyPrint(e)
+      case Len(e) => "len " + prettyPrint(e)
+      case Ord(e) => "ord " + prettyPrint(e)
+      case Chr(e) => "chr " + prettyPrint(e)
+      case ArrayLiter(exprs) =>
+        val sb = new StringBuilder("]")
+        if (exprs.nonEmpty) {
+          if (exprs.length > 1) {
+            for (i <- exprs.indices) {
+              sb.append(exprs(i))
+              sb += ','
+            }
+          }else{
+            sb.append(exprs.head)
+          }
+        }
+        sb +=']'
+        sb.toString()
+      case _ => ""
+    }
+  }
+
+}
