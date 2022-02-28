364e9a3686c5bb33f796f58fda7b8adcf99e70f1
James Lee-Jones <jl8320@ic.ac.uk>
2022-02-28 12:40:09 +0000

    [jl8320, sg520, lsm20, ac320] Began to implement RegisterAllocator and added new cases to CodeGen.

--- src/main/scala/parsers/CodeGen.scala
+++ src/main/scala/parsers/CodeGen.scala
@@ -1,6 +1,7 @@
 package parsers
 
 import scala.collection.mutable
+import scala.collection.immutable
 import scala.collection.mutable.ListBuffer
 
 object CodeGen{
@@ -17,7 +18,7 @@ object CodeGen{
   // todo: refactor so that maps/buffers automatically indent/format strings
 
   //TODO make use of availableRegs to replace
-  def traverse(node: AstNode, availableRegs: mutable.ListBuffer[Int]): Unit = {
+  def traverse(node: AstNode, ra: RegisterAllocator): String = {
     node match {
       case Program(funcs, stat) =>
 
@@ -39,18 +40,28 @@ object CodeGen{
       case Param(_type, ident) =>
 
       // <Stat>
-      case Skip =>
-      case Decl(PairType(t1, t2), ident, PairLiter) =>
-      case Decl(_type, ident, rhs) =>
+      case Skip => ???
 
-      case Assign(lhs, rhs) =>
-      case Read(lhs) =>
-      case Free(expr) =>
+      case Decl(PairType(t1, t2), ident, PairLiter) => ???
+      case Decl(_type, ident, rhs) =>
+        // TODO this is not gonna work (see many variables example)
+        val reg = ra.next()
+        "SUB sp, sp, #4" +
+        "LDR " + reg +", =" + traverse(rhs, ra) + ////////////////////////////////////////
+        "STR " + reg + ", [sp]" +
+        "ADD sp, sp, #4"
+
+      case Assign(lhs, rhs) => ???
+
+      case Read(lhs) => ???
+
+      case Free(expr) => ???
+
       case Print(expr) =>
         val _type: Type = SemanticPass.checkExprType(expr, node, new ListBuffer[String])
         _type match{
           case WString =>
-            main += "LDR r1, [r0]" +
+            "LDR r1, [r0]" +
             "ADD r2, r0, #4" +
             "LDR r0, =msg_3" +
             "ADD r0, r0, #4" +
@@ -58,11 +69,7 @@ object CodeGen{
             "MOV r0, #0" +
             "BL fflush"
           case WBool =>
-            main += "MOV r4, #0" + // todo: r4 might not be available
-              "MOV r0, r4" +
-              "BL p_print_bool"
-            functions += "p_print_bool:" +
-              // todo: needs to check if function is already there
+            add(functions, "p_print_bool:",
                 "PUSH {lr}" +
                 "CMP r0, #0" +
                 "LDRNE r0, =msg_0" +
@@ -71,47 +78,82 @@ object CodeGen{
                 "BL printf" +
                 "MOV r0, #0" +
                 "BL fflush" +
-                "POP {pc}"
+                "POP {pc}")
+
+            "MOV r4, #0" + // todo: r4 might not be available
+            "MOV r0, r4" +
+            "BL p_print_bool"
             //TODO: all other print types
           //TODO: find all functions that are branched to and add
           //TODO: add global messages that can be added
         }
-      case Println(expr) => // todo: mostly same as print
-      case Return(expr) =>
+
+      case Println(expr) => ??? // todo: mostly same as print
+
+      case Return(expr) => ??? //todo
+
       case Exit(expr) =>
+        val reg = ra.next()
+        "LDR " + reg + " =" + traverse(expr, ra) +
+        "MOV r0, r4" +
+        "BL exit"
+
       case IfElse(cond, stat_true, stat_false) =>
-        traverse(cond, availableRegs)
-        main += "CMP r4, #0\n"
-        traverse(stat_true, availableRegs)
-        main += "L0:\n"
-        traverse(stat_false, availableRegs)
-        main += "L1:\n"
+        // TODO add stack pointer changes for new scopes
+        val reg = ra.next()
+        traverse(cond, ra)
+        var result = "CMP " + reg + ", #0\n"
+        val newScope = new RegisterAllocator(ra.getAvailable)
+        traverse(stat_true, newScope)
+        newScope.restore()
+        result += "L0:\n"
+        traverse(stat_false, newScope)
+        result + "L1:\n"
+
+
       case While(cond, stat) =>
-        traverse(stat, availableRegs)
-      case Scope(stat) => traverse(stat, availableRegs)
-      case Combine(stats) => stats.foreach(traverse(_, availableRegs))
+        // TODO add stack changes for scoping
+        "B L0" +
+          "L1:" + traverse(stat, ra) +
+        "L0" + traverse(cond, ra)
+        //TODO GOT TO HERE WITH RA
+
+      case Scope(stat) => traverse(stat, ra)
+
+      case Combine(stats) =>
+        // TODO this could be refactored
+        val statements = ""
+        for (stat <- stats) {
+          statements + traverse(stat, ra)
+        }
+        statements
+
       case Or(BoolLiter(_), BoolLiter(_)) =>
         main += "MOV r4, #1" +
           "MOV r5, #0" +
           "ORR r4, r4, r5" +
           "MOV r0, r4"
+
       case Or(expr1, expr2) =>
-        traverse(expr1, availableRegs)
-        traverse(expr2, availableRegs)
-        main += "ORR r4, r4, r5" +
+        traverse(expr1, ra)
+        traverse(expr2, ra)
+        "ORR r4, r4, r5" +
           "MOV r0, r4"
       //TODO: print function but only if not already there
+
       case And(BoolLiter(_), BoolLiter(_)) =>
         main += "MOV r4, #1" +
           "MOV r5, #0" +
           "AND r4, r4, r5" +
           "MOV r0, r4"
+
       case And(expr1, expr2) =>
-        traverse(expr1, availableRegs)
-        traverse(expr2, availableRegs)
-        main += "AND r4, r4, r5" +
+        traverse(expr1, ra)
+        traverse(expr2, ra)
+        "AND r4, r4, r5" +
           "MOV r0, r4"
       //TODO: print function but only if not already there
+
       case Greater(expr1, expr2) =>
         traverse(expr1, availableRegs)
         traverse(expr2, availableRegs)
@@ -160,6 +202,21 @@ object CodeGen{
 
   //helper functions to make code cleaner
 
+  val typeSize: immutable.Map[Type, Int] = Map[Type, Int](WInt -> 4, WBool -> 1, WChar -> 1)
+
+  def assignmentsInScope(stats: Stat): Int = {
+    var size = 0;
+    stats match {
+      case Decl(_type, _, _) => size += typeSize(_type)
+      case Combine(stats) =>
+        for (stat <- stats) {
+          stat match {
+            case Decl(_type, _, _) => size += typeSize(_type)
+          }
+        }
+    }
+    size
+  }
 
   def pop(availableRegs: ListBuffer[Int]): String = {
     /**
---
+++ src/main/scala/parsers/RegisterAllocator.scala
@@ -0,0 +1,37 @@
+package parsers
+import scala.collection.mutable.ListBuffer
+
+class RegisterAllocator(availableRegisters: ListBuffer[Int]) {
+
+  val allRegisters = List(4, 5, 6, 7, 8, 9, 10, 11, 12)
+  val initialAvailable = availableRegisters.clone()
+  //TODO check .clone does a deep copy, ie modifying availableRegs doesnt modify initialAvailable
+  //reserved registers
+  val retReg = "r0"
+  val sp = "r13"
+  val linkReg = "r14"
+  val pc = "r15"
+
+  //if no list buffer in constructor, assume we have access to all registers
+  def this() = {
+    this(allRegisters.to(ListBuffer))
+  }
+
+
+  def next(): String = {
+    val reg = "r" + availableRegisters.head.toString
+    availableRegisters.remove(0)
+    reg
+  }
+
+  def restore(): Unit = {
+    val used = initialAvailable.diff(availableRegisters)
+    availableRegisters.addAll(used)
+  }
+
+  def getAvailable: ListBuffer[Int] = {
+    availableRegisters
+  }
+
+
+}
