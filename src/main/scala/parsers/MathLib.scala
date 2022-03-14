f293fd5031a0ebe45af644bdf6b198e41ea1097a
sg520 <sg520@ic.ac.uk>
2022-03-14 22:58:36 +0000

    [sg520] all trig

--- src/main/scala/parsers/MathLib.scala
+++ src/main/scala/parsers/MathLib.scala
@@ -7,129 +7,256 @@ import parsers.preDefinedFuncs._
 
 class MathLib {
 
+  //TODO: add references to branched functions
+
   val mathFuncs = List("acos", "asin", "atan", "atan2", "cos", "cosh", "sin", "sinh",
     "tanh", "exp", "frexp", "ldexp", "log", "log10", "modf", "pow",
     "sqrt", "ceil", "fabs", "floor", "fmod")
 
-  def mathCodeGen(ident: String, value: Double, ra: RegisterAllocator) ={
-    ident match{
-      case "asin" =>
-        var r = ra.next
-        val condLabel = nextBranchIndex
-        val bodyLabel = nextBranchIndex
-        intOverflow()
-        divByZeroError()
-        runtimeError()
-        printString()
-        //work out how to add fabs code
-        if (!labels.contains("asin")){
-          labels("asin") = List(PUSH(LinkReg),
-            SUB(SP, SP, imm(24)),
-            LDR(r, imm(1), Base),
-            STR(r, regShift(SP, 20, false)),
-            LDR(r, regShift(SP, 28, false), Base),
-            STR(r, regShift(SP, 16, false)),
-            LDR(r, imm(1), Base),
-            STR(r, regShift(SP, 12, false)),
-            LDR(r, imm(1), Base),
-            STR(r, regShift(SP, 8, false)),
-            LDR(r, regShift(SP, 28, false), Base),
-            STR(r, regShift(SP, 4, false)),
-            LDR(r, regShift(SP, 12, false), Base),
-            STR(r, regShift(SP, -4, true)),
-            BL("fabs", Base),
-            ADD(SP, SP, imm(4)),
-            MOV(r, RetReg, Base),
-            STR(r, regVal(SP)),
-            B(condLabel, Base)
-          )
-          val r1 = ra.next
-          val r2 = ra.next
-          labels(condLabel) = List(LDR(r, regVal(SP), Base),
-            //this won't work
-            LDR(r1, label("0.0000001"), Base),
-            CMP(r, r1),
-            MOV(r, imm(1), GT),
-            MOV(r, imm(0), LE),
-            LDR(r1, regShift(SP, 20, false), Base),
-            LDR(r2, imm(100), Base),
-            CMP(r1, r2),
-            MOV(r1, imm(1), LT),
-            MOV(r2, imm(0), GE),
-            AND(r, r, r1),
-            CMP(r, imm(1)),
-            B(bodyLabel, EQ),
-            LDR(r, regShift(SP, 16, false), Base),
-            MOV(RetReg, r, Base),
-            ADD(SP, SP, imm(24)),
-            POP(PC),
-            POP(PC),
-            LTORG
-          )
-          val r3 = ra.next
-          labels(bodyLabel) = List(LDR(r, regShift(SP, 8, false), Base),
-            LDR(r1, imm(2), Base),
-            LDR(r2, regShift(SP, 20, false), Base),
-            SMULL(r1, r2, r1, r2),
-            CMP(r2, asr(r1, 31)),
-            BL("p_throw_overflow_error", NE),
-            LDR(r2, imm(2), Base),
-            LDR(r3, regShift(SP, 20, false), Base),
-            SMULL(r2, r3, r2, r3),
-            CMP(r3, asr(r2, 31)),
-            BL("p_throw_overflow_error", NE),
-            LDR(r3, imm(1), Base),
-            ADDS(r2, r2, r3),
-            BL("p_throw_overflow_error", VS),
-            SMULL(r1, r2, r1, r2),
-            CMP(r2, asr(r1, 31)),
-            BL("p_throw_overflow_error", NE),
-            SMULL(r, r1, r, r1),
-            CMP(r1, asr(r, 31)),
-            BL("p_throw_overflow_error", NE),
-            STR(r, regShift(SP, 8, false)),
-            LDR(r, regShift(SP, 4, false), Base),
-            LDR(r1, imm(-1), Base),
-            SMULL(r, r1, r, r1),
-            CMP(r1, asr(r, 31)),
-            BL("p_throw_overflow_error", NE),
-            LDR(r1, regShift(SP, 28, false), Base),
-            SMULL(r, r1, r, r1),
-            CMP(r1, asr(r, 31)),
-            BL("p_throw_overflow_error", NE),
-            LDR(r1, regShift(SP, 28, false), Base),
-            SMULL(r, r1, r, r1),
-            CMP(r1, asr(r, 31)),
-            BL("p_throw_overflow_error", NE),
-            STR(r, regShift(SP, 4, false)),
-            LDR(r, regShift(SP, 4, false), Base),
-            LDR(r1, regShift(SP, 8, false), Base),
-            MOV(RetReg, r, Base),
-            MOV(reg(1), r1, Base),
-            BL("p_check_divide_by_zero", Base),
-            BL("__aeabi_idiv", Base),
-            MOV(r, RetReg, Base),
-            STR(r, regShift(SP, 12, false)),
-            LDR(r, regShift(SP, 16, false), Base),
-            LDR(r1, regShift(SP, 12, false), Base),
-            ADDS(r, r, r1),
-            BL("p_throw_overflow_error", VS),
-            STR(r, regShift(SP, 16, false)),
-            LDR(r, regShift(SP, 20, false), Base),
-            LDR(r1, imm(1), Base),
-            ADDS(r, r, r1),
-            BL("p_throw_overflow_error", VS),
-            STR(r, regShift(SP, 20, false)),
-            LDR(r, regShift(SP, 12, false), Base),
-            STR(r, regShift(SP, -4, true)),
-            BL("fabs", Base),
-            ADD(SP, SP, imm(4)),
-            MOV(r, RetReg, Base),
-            STR(r, regVal(SP))
-          )
-        }
-      case "fabs" =>
+  def asin(x: Int, ra: RegisterAllocator) {
+    var r = ra.next
+    val condLabel = nextBranchIndex
+    val bodyLabel = nextBranchIndex
+    intOverflow()
+    divByZeroError()
+    runtimeError()
+    printString()
+    //work out how to add fabs code
+    if (!labels.contains("asin")) {
+      labels("asin") = List(PUSH(LinkReg),
+        SUB(SP, SP, imm(24)),
+        LDR(r, imm(1), Base),
+        STR(r, regShift(SP, 20, false)),
+        LDR(r, regShift(SP, 28, false), Base),
+        STR(r, regShift(SP, 16, false)),
+        LDR(r, imm(1), Base),
+        STR(r, regShift(SP, 12, false)),
+        LDR(r, imm(1), Base),
+        STR(r, regShift(SP, 8, false)),
+        LDR(r, regShift(SP, 28, false), Base),
+        STR(r, regShift(SP, 4, false)),
+        LDR(r, regShift(SP, 12, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        BL("fabs", Base),
+        ADD(SP, SP, imm(4)),
+        MOV(r, RetReg, Base),
+        STR(r, regVal(SP)),
+        B(condLabel, Base)
+      )
+      val r1 = ra.next
+      val r2 = ra.next
+      labels(condLabel) = List(LDR(r, regVal(SP), Base),
+        //this won't work
+        LDR(r1, label("0.0000001"), Base),
+        CMP(r, r1),
+        MOV(r, imm(1), GT),
+        MOV(r, imm(0), LE),
+        LDR(r1, regShift(SP, 20, false), Base),
+        LDR(r2, imm(100), Base),
+        CMP(r1, r2),
+        MOV(r1, imm(1), LT),
+        MOV(r2, imm(0), GE),
+        AND(r, r, r1),
+        CMP(r, imm(1)),
+        B(bodyLabel, EQ),
+        LDR(r, regShift(SP, 16, false), Base),
+        MOV(RetReg, r, Base),
+        ADD(SP, SP, imm(24)),
+        POP(PC),
+        POP(PC),
+        LTORG
+      )
+      val r3 = ra.next
+      labels(bodyLabel) = List(LDR(r, regShift(SP, 8, false), Base),
+        LDR(r1, imm(2), Base),
+        LDR(r2, regShift(SP, 20, false), Base),
+        SMULL(r1, r2, r1, r2),
+        CMP(r2, asr(r1, 31)),
+        BL("p_throw_overflow_error", NE),
+        LDR(r2, imm(2), Base),
+        LDR(r3, regShift(SP, 20, false), Base),
+        SMULL(r2, r3, r2, r3),
+        CMP(r3, asr(r2, 31)),
+        BL("p_throw_overflow_error", NE),
+        LDR(r3, imm(1), Base),
+        ADDS(r2, r2, r3),
+        BL("p_throw_overflow_error", VS),
+        SMULL(r1, r2, r1, r2),
+        CMP(r2, asr(r1, 31)),
+        BL("p_throw_overflow_error", NE),
+        SMULL(r, r1, r, r1),
+        CMP(r1, asr(r, 31)),
+        BL("p_throw_overflow_error", NE),
+        STR(r, regShift(SP, 8, false)),
+        LDR(r, regShift(SP, 4, false), Base),
+        LDR(r1, imm(-1), Base),
+        SMULL(r, r1, r, r1),
+        CMP(r1, asr(r, 31)),
+        BL("p_throw_overflow_error", NE),
+        LDR(r1, regShift(SP, 28, false), Base),
+        SMULL(r, r1, r, r1),
+        CMP(r1, asr(r, 31)),
+        BL("p_throw_overflow_error", NE),
+        LDR(r1, regShift(SP, 28, false), Base),
+        SMULL(r, r1, r, r1),
+        CMP(r1, asr(r, 31)),
+        BL("p_throw_overflow_error", NE),
+        STR(r, regShift(SP, 4, false)),
+        LDR(r, regShift(SP, 4, false), Base),
+        LDR(r1, regShift(SP, 8, false), Base),
+        MOV(RetReg, r, Base),
+        MOV(reg(1), r1, Base),
+        BL("p_check_divide_by_zero", Base),
+        BL("__aeabi_idiv", Base),
+        MOV(r, RetReg, Base),
+        STR(r, regShift(SP, 12, false)),
+        LDR(r, regShift(SP, 16, false), Base),
+        LDR(r1, regShift(SP, 12, false), Base),
+        ADDS(r, r, r1),
+        BL("p_throw_overflow_error", VS),
+        STR(r, regShift(SP, 16, false)),
+        LDR(r, regShift(SP, 20, false), Base),
+        LDR(r1, imm(1), Base),
+        ADDS(r, r, r1),
+        BL("p_throw_overflow_error", VS),
+        STR(r, regShift(SP, 20, false)),
+        LDR(r, regShift(SP, 12, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        BL("fabs", Base),
+        ADD(SP, SP, imm(4)),
+        MOV(r, RetReg, Base),
+        STR(r, regVal(SP))
+      )
     }
   }
 
+  def acos(x:Int, ra: RegisterAllocator): Unit ={
+    divByZeroError()
+    intOverflow()
+    runtimeError()
+    printString()
+    val condLabel = nextBranchIndex
+    val bodyLabel = nextBranchIndex
+    if (!labels.contains("acos")){
+      val r = ra.next
+      labels("acos") = List(
+        PUSH(LinkReg),
+        SUB(SP, SP, imm(12)),
+        LDR(r, imm(1), Base),
+        STR(r, regShift(SP, 8, false)),
+        LDR(r, imm(-1), Base),
+        STR(r, regShift(SP, 4, false)),
+        LDR(r, imm(2), Base),
+        STR(r, regVal(SP)),
+        B(condLabel, Base)
+      )
+      val r1 = ra.next
+      labels(condLabel) = List(
+        LDR(r, regVal(SP), Base),
+        LDR(r1, imm(100), Base),
+        CMP(r, r1),
+        MOV(r, imm(1), LE),
+        MOV(r, imm(0), GT),
+        CMP(r, imm(1)),
+        B(bodyLabel, EQ),
+        LDR(r, regShift(SP, 8, false), Base),
+        MOV(RetReg, r, Base),
+        ADD(SP, SP, imm(12)),
+        POP(PC),
+        POP(PC),
+        LTORG
+      )
+      val r2 = ra.next
+      labels(bodyLabel) = List(
+        SUB(SP, SP, imm(16)),
+        LDR(r, regShift(SP, 16, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        LDR(r, regShift(SP, 36, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        BL("pow", Base),
+        ADD(SP, SP, imm(8)),
+        MOV(r, RetReg, Base),
+        STR(r, regShift(SP, 12, false)),
+        LDR(r, regShift(SP, 16, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        BL("fact", Base),
+        ADD(SP, SP, imm(4)),
+        MOV(r, RetReg, Base),
+        STR(r, regShift(SP, 8, false)),
+        LDR(r, regShift(SP, 12, false), Base),
+        LDR(r1, regShift(SP, 8, false), Base),
+        MOV(RetReg, r, Base),
+        MOV(reg(1), r1, Base),
+        BL("p_check_divide_by_zero", Base),
+        BL("__aeabi_idiv", Base),
+        MOV(r, RetReg, Base),
+        STR(r, regShift(SP, 4, false)),
+        LDR(r, regShift(SP, 20, false), Base),
+        LDR(r1, regShift(SP, 4, false), Base),
+        SMULL(r, r1, r, r1),
+        CMP(r1, asr(r, 31)),
+        BL("p_throw_overflow_error", NE),
+        STR(r, regVal(SP)),
+        LDR(r, regShift(SP, 24, false), Base),
+        LDR(r1, regVal(SP), Base),
+        ADDS(r, r, r1),
+        BL("p_throw_overflow_error", VS),
+        STR(r, regShift(SP, 24, false)),
+        LDR(r, regShift(SP, 20, false), Base),
+        LDR(r1, imm(-1), Base),
+        SMULL(r, r1, r, r1),
+        CMP(r1, asr(r, 31)),
+        BL("p_throw_overflow_error", NE),
+        STR(r, regShift(SP, 20, false)),
+        LDR(r, regShift(SP, 16, false), Base),
+        LDR(r1, imm(2), Base),
+        ADDS(r, r, r1),
+        BL("p_throw_overflow_error", NE),
+        STR(r, regShift(SP, 20, false)),
+        ADD(SP, SP, imm(16))
+      )
+    }
+  }
+
+  def atan(x: Int, ra:RegisterAllocator): Unit ={
+    printString()
+    runtimeError()
+    divByZeroError()
+    acos(x, ra)
+    asin(x, ra)
+    if(!labels.contains("atan")){
+      val r = ra.next
+      val r1 = ra.next
+      labels("atan") = List(
+        PUSH(LinkReg),
+        SUB(SP, SP, imm(12)),
+        LDR(r, regShift(SP, 16, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        BL("sin", Base),
+        ADD(SP, SP, imm(4)),
+        MOV(r, RetReg, Base),
+        STR(r, regShift(SP,8, false)),
+        LDR(r, regShift(SP, 16, false), Base),
+        STR(r, regShift(SP, -4, true)),
+        BL("cos", Base),
+        ADD(SP, SP, imm(4)),
+        MOV(r, RetReg, Base),
+        STR(r, regShift(SP, 4, false)),
+        LDR(r, regShift(SP, 8, false), Base),
+        LDR(r1, regShift(SP, 4, false), Base),
+        MOV(RetReg, r, Base),
+        MOV(reg(1), r1, Base),
+        BL("p_check_divide_by_zero", Base),
+        BL("__aeabi_idiv", Base),
+        MOV(r, RetReg, Base),
+        STR(r, regVal(SP)),
+        LDR(r, regVal(SP), Base),
+        MOV(RetReg, r, Base),
+        ADD(SP, SP, imm(12)),
+        POP(PC),
+        POP(PC),
+        LTORG
+      )
+    }
+  }
 }
